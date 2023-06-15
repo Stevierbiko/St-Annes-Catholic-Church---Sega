9814f9fe7cd26963325c700adb65566eb043e4d4
stevier biko <stevierbiko@gmail.com>
2023-06-15 11:07:47 +0300

    user experience

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const hambuger = document.querySelector('.hambuger');
-const navMenu = document.querySelector('.nav-menu');
+const hambuger = document.querySelector(".hambuger");
+const navMenu = document.querySelector(".nav-menu");
 
 hambuger.addEventListener("click", mobileMenu);
 
@@ -8,7 +8,7 @@ function mobileMenu() {
   navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll('.nav-link');
+const navLink = document.querySelectorAll(".nav-link");
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
@@ -18,25 +18,25 @@ function closeMenu() {
 
 // Hero section Script
 function img(anything) {
-  document.querySelector('.slide').src = anything;
+  document.querySelector(".slide").src = anything;
 }
 
 function change(change) {
-  const line = document.querySelector('.image');
+  const line = document.querySelector(".image");
   line.style.background = change;
 }
 
-previewBox.forEach(close => {
-  close.querySelector('.fa-times').onclick = () => {
-    close.classList.remove('active');
-    preveiwContainer.style.display = 'none';
+previewBox.forEach((close) => {
+  close.querySelector(".fa-times").onclick = () => {
+    close.classList.remove("active");
+    preveiwContainer.style.display = "none";
   };
 });
 
-//OUR Staff 
+//OUR Staff
 var swiper = new Swiper(".staff-slider", {
-  loop:true,
-  grabCursor:true,
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -52,36 +52,38 @@ var swiper = new Swiper(".staff-slider", {
 });
 
 // Sacraments accodion Script
-var accItem = document.getElementsByClassName('accordionItem');
-var accHD = document.getElementsByClassName('accordionIHeading');
+var accItem = document.getElementsByClassName("accordionItem");
+var accHD = document.getElementsByClassName("accordionIHeading");
 
 for (i = 0; i < accHD.length; i++) {
-  accHD[i].addEventListener('click', toggleItem, false);
+  accHD[i].addEventListener("click", toggleItem, false);
 }
 
 function toggleItem() {
   var itemClass = this.parentNode.className;
   for (var i = 0; i < accItem.length; i++) {
-    accItem[i].className = 'accordionItem close';
+    accItem[i].className = "accordionItem close";
   }
-  if (itemClass == 'accordionItem close') {
-    this.parentNode.className = 'accordionItem open';
+  if (itemClass == "accordionItem close") {
+    this.parentNode.className = "accordionItem open";
   }
 }
 
 // Faq Accordion
-let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+let accordion = document.querySelectorAll(
+  ".faq .accordion-container .accordion"
+);
 
-accordion.forEach(acco => {
+accordion.forEach((acco) => {
   acco.onclick = () => {
-    accordion.forEach(dion => dion.classList.remove('active'));
-    acco.classList.toggle('active');
+    accordion.forEach((dion) => dion.classList.remove("active"));
+    acco.classList.toggle("active");
   };
 });
 
-document.querySelector('.load-more .btn').onclick = () => {
-  document.querySelectorAll('.parish .box-container .hide').forEach(show => {
-    show.style.display = 'block';
+document.querySelector(".load-more .btn").onclick = () => {
+  document.querySelectorAll(".parish .box-container .hide").forEach((show) => {
+    show.style.display = "block";
   });
-  document.querySelector('.load-more .btn').style.display = 'none';
+  document.querySelector(".load-more .btn").style.display = "none";
 };
